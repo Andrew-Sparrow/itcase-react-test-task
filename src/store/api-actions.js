c136704b87e3129ffe05c6a129efbac7cc9f266a
Andrew <8911102@gmail.com>
2023-02-24 16:01:25 +0500

    api-actions.js with getProducts was added

---
+++ src/store/api-actions.js
@@ -0,0 +1,15 @@
+import {
+  loadProducts,
+  redirectToRoute
+} from './actions'
+
+import { getProducts } from '../services/api';
+
+
+export const fetchProductsList = () => (dispatch, _getState) => (
+  getProducts()
+    .then(({data}) => {
+      dispatch(loadProducts(data));
+    })
+    .catch((err) => {})
+)
--- src/store/products/selectors.js
+++ src/store/products/selectors.js
@@ -1,4 +1,4 @@
 import { NameSpace } from '../root-reducer'
 
 export const getPlaces = (state) => state[NameSpace.PRODUCTS].products
-export const getIsProductsLoaded = (state) => state[NameSpace.PRODUCTS].getIsProductsLoaded
+export const getIsProductsLoaded = (state) => state[NameSpace.PRODUCTS].isProductsLoaded
