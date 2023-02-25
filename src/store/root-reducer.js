6c583710e12e7a88d4ab5bb6413c6a5b8494c134
Andrew <8911102@gmail.com>
2023-02-25 14:01:32 +0500

    sizes reducer was added to combine reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,14 +1,17 @@
 import {combineReducers} from 'redux'
 import { products } from './products/products'
 import { product } from './product-card/product-card';
+import { sizes } from './sizes/sizes';
 
 
 export const NameSpace = {
   PRODUCTS: 'PRODUCTS',
   PRODUCT_CARD: 'PRODUCT_CARD',
+  SIZES: 'SIZES',
 };
 
 export default combineReducers({
   [NameSpace.PRODUCTS]: products,
   [NameSpace.PRODUCT_CARD]: product,
+  [NameSpace.SIZES]: sizes,
 })
