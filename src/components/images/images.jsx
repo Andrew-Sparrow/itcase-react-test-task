a4522692dbca3b6dde4aba3f6936a4f17012b51b
Andrew <8911102@gmail.com>
2023-02-25 23:24:02 +0500

    ability to change picture by click was added

--- src/components/images/images.jsx
+++ src/components/images/images.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './images.scss';
 
 
 function Images({ images }) {
+  const [activeImgIndex, setActiveImgIndex] = useState(0)
+
+  function handleClickImg(evt) {
+    setActiveImgIndex(+evt.target.id)
+  }
+
   return (
     <div>
       <ul className='images'>
@@ -11,17 +17,19 @@ function Images({ images }) {
           ? images.map((imgPath, index) =>
             <li className='images__point' key={index}>
               <img
+                id={index}
                 className='images__item images__item--active'
                 src={imgPath}
                 alt='Продукт'
                 key={index}
+                onClick={handleClickImg}
               />
             </li>) : ''}
       </ul>
       {(images.length !== 0) ?
         <img
           className='images__picture'
-          src={images[0]}
+          src={images[activeImgIndex]}
           alt="продукт"
         />
         : ''}
